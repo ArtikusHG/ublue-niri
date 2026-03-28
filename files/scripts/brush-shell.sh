31bf44a97d39c751a400ab67c16f4afbe6b32a4a
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2026-03-28 14:34:14 +0200

    Update brush-shell.sh

--- files/scripts/brush-shell.sh
+++ files/scripts/brush-shell.sh
@@ -2,6 +2,6 @@
 
 curl -LO $(curl -s https://api.github.com/repos/reubeno/brush/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.gz')
 tar -xzf brush-x86_64-unknown-linux-gnu.tar.gz
-cp brush-x86_64-unknown-linux-gnu/brush /usr/bin/
+cp brush /usr/bin/
 chmod +x /usr/bin/brush
 ln -sf /usr/bin/brush /usr/bin/bash
