a165695d67918f3d0be3dfe8e164128d62306e4f
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2026-03-28 10:44:38 +0200

    Update install-helium.sh

--- files/scripts/install-helium.sh
+++ files/scripts/install-helium.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl -L $(curl -s https://api.github.com/repos/imputnet/helium-linux/releases/latest | grep -o -m1 'https://[^"]*x86_64\.AppImage') -o /usr/bin/helium
-/usr/bin/helium --appimage-extract
-cp squashfs-root/usr/share/icons/hicolor/256x256/apps/helium.png /usr/share/icons/hicolor/256x256/apps/
-rm -rf squashfs-root
+#/usr/bin/helium --appimage-extract
+#cp squashfs-root/usr/share/icons/hicolor/256x256/apps/helium.png /usr/share/icons/hicolor/256x256/apps/
+#rm -rf squashfs-root
