60c3a5601f7e78fbc586828715fff607d14ce90b
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2026-03-28 10:22:43 +0200

    Update install-helium.sh

--- files/scripts/install-helium.sh
+++ files/scripts/install-helium.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -L $(curl -s https://api.github.com/repos/imputnet/helium-linux/releases/latest | grep -o -m1 'https://[^"]*x86_64\.AppImage') -o /usr/local/bin/helium
+curl -L $(curl -s https://api.github.com/repos/imputnet/helium-linux/releases/latest | grep -o -m1 'https://[^"]*x86_64\.AppImage') -o /usr/bin/helium
