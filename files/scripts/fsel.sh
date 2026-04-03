06cce1ec9183e56be3d2f887da698d509e628e5e
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2026-04-03 09:48:49 +0300

    Update fsel.sh

--- files/scripts/fsel.sh
+++ files/scripts/fsel.sh
@@ -2,5 +2,5 @@
 
 curl -LO $(curl -s https://api.github.com/repos/Mjoyufull/fsel/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.xz')
 tar -xf fsel-x86_64-unknown-linux-gnu.tar.xz
-cp fsel /usr/bin/
+cp fsel*/fsel /usr/bin/
 chmod +x /usr/bin/fsel
