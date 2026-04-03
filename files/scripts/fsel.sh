59f6db1089913d8a44045cbc0fc3003fe7de7d49
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2026-04-03 09:35:03 +0300

    Create fsel.sh

---
+++ files/scripts/fsel.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -LO $(curl -s https://api.github.com/repos/Mjoyufull/fsel/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.xz')
+tar -xzf fsel-x86_64-unknown-linux-gnu.tar.xz
+cp fsel /usr/bin/
+chmod +x /usr/bin/fsel
