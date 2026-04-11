151715fed55ff378ac092e52e0724b43afba6ce0
ArtikusHG <24320212+ArtikusHG@users.noreply.github.com>
2026-04-11 23:46:24 +0300

    Update uutils-findutils.sh

--- files/scripts/uutils-findutils.sh
+++ files/scripts/uutils-findutils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -LO $(curl -s https://api.github.com/repos/uutils/findutils/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.xz')
+curl -LO $(curl -s https://api.github.com/repos/uutils/findutils/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.xz' | head -n1)
 tar -xf findutils-x86_64-unknown-linux-gnu.tar.xz
 cp find /usr/bin/
 cp xargs /usr/bin
