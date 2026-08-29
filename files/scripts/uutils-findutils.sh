a5dc92c78bd1be74c67752641581c2e99b632215
ArtikusHG <github.mr535@aleeas.com>
2026-08-29 22:27:38 +0300

    Delete files/scripts/uutils-findutils.sh

--- files/scripts/uutils-findutils.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -LO $(curl -s https://api.github.com/repos/uutils/findutils/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.xz' | head -n1)
-tar -xf findutils-x86_64-unknown-linux-gnu.tar.xz
-cp findutils*/find /usr/bin/
-cp findutils*/xargs /usr/bin
-chmod +x /usr/bin/find
-chmod +x /usr/bin/xargs
