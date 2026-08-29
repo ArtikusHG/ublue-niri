efb85349819614f5eaf0383e1ae2b2fa1b725c0f
ArtikusHG <github.mr535@aleeas.com>
2026-08-29 22:27:15 +0300

    Delete files/scripts/fsel.sh

--- files/scripts/fsel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -LO $(curl -s https://api.github.com/repos/Mjoyufull/fsel/releases/latest | grep -o 'https://[^"]*x86_64-unknown-linux-gnu\.tar\.xz' | head -n1)
-tar -xf fsel-x86_64-unknown-linux-gnu.tar.xz
-cp fsel*/fsel /usr/bin/
-chmod +x /usr/bin/fsel
